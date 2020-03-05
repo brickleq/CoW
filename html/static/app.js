40bf185586e8105757adfd557cb8b1cbe9b13b1c
brickleq <pblequire@gmail.com>
2020-03-04 23:45:56 -0600

    add leaflet viz page

---
+++ html/static/app.js
@@ -0,0 +1,9 @@
+var mymap = L.map('mapid').setView([51.505, -0.09], 1.75);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/satellite-v9',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: MapBoxToken
+}).addTo(mymap);
