633a8efeaf292df53c43566eb436753cc22216c8
brickleq <pblequire@gmail.com>
2020-03-10 16:55:42 -0500

    add queries.sql

---
+++ queries.sql
@@ -0,0 +1,11 @@
+/* Retrieve MIDA and MIDLOCA data for major powers */
+select m.dispnum3, stabb, ccode, x.*
+from midp as m
+inner join
+(select * from midloca
+inner join mida
+on midloca.dispnum = mida.dispnum3) as x
+on m.dispnum3 = x.dispnum3
+where m.ccode in 
+(select ccode from majors2016)
+order by year asc;
