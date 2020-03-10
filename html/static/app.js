cfadae6bbfa6bad4a8b1dc70950532407eb0e65e
brickleq <pblequire@gmail.com>
2020-03-10 14:21:05 -0500

    format Leaflet display

--- html/static/app.js
+++ html/static/app.js
@@ -1,6 +1,6 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 1.75);
+var mymap = L.map('mapid').setView([31.505, 10.91], 1.75);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/satellite-v9',
     tileSize: 512,
