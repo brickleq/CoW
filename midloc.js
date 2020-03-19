3838b30f4faf164c0ea1b51a739b5068db144e07
brickleq <pblequire@gmail.com>
2020-03-19 16:23:21 -0500

    edit midloc.js

--- midloc.js
+++ midloc.js
@@ -1,5 +1,6 @@
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyMGZjYzI5OS0wZDQ2LTRkMTctOGY0Yi1lZWE3Y2U4ZTQ5ZDEiLCJpZCI6MTI1MjUsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NjEyMjIwMjl9.swoqaLkpdCJ0g8USzCJr_z5k1md3UvPdpMqleOUHyL4';
 var viewer = new Cesium.Viewer('cesiumContainer', {
+    terrainProvider: Cesium.createWorldTerrain(),
     animation: false,
     baseLayerPicker: false,
     fullscreenButton: false,
@@ -9,3 +10,5 @@ var viewer = new Cesium.Viewer('cesiumContainer', {
     sceneModePicker: false,
     timeline: true,
   });
+
+  
